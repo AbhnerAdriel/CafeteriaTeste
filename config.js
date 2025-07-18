0ee4de9826e15238f8e70c75b870e92c62aa8188
Abhner Adriel <86312332+AbhnerAdriel@users.noreply.github.com>
2025-07-18 15:48:56 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// URLs das planilhas Google Sheets
+const CONFIG_PLANILHAS = {
+
+    itens: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTEs-_XTzIOURiafsIsHBfgPqeh4gEy9mMlKCCkYhpKM_o2AjqAEUAEsEag8HiFGeC_WmTCzBxe4vv0/pub?gid=0&single=true&output=csv',
+
+    categorias: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTEs-_XTzIOURiafsIsHBfgPqeh4gEy9mMlKCCkYhpKM_o2AjqAEUAEsEag8HiFGeC_WmTCzBxe4vv0/pub?gid=2055726394&single=true&output=csv',
+
+    config: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTEs-_XTzIOURiafsIsHBfgPqeh4gEy9mMlKCCkYhpKM_o2AjqAEUAEsEag8HiFGeC_WmTCzBxe4vv0/pub?gid=1954059800&single=true&output=csv'
+};
